59169faaef2f1488b8c50e43e272be63c6b0933e
ComicPuzzle <alphadjw@gmail.com>
2024-08-28 18:24:16 -0400

    Getting graph points and test

--- Lab2/BlinkLED.py
+++ Lab2/BlinkLED.py
@@ -1,32 +1,42 @@
 import RPi.GPIO as GPIO
+import time
 from time import sleep
-
+'''
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(1, GPIO.OUT, initial=GPIO.LOW) #replace 1 with pin number
-while True:
-    GPIO.output(1, GPIO.HIGH)
-    sleep(0.5) #in seconds
-    GPIO.output(1, GPIO.LOW)
+GPIO.setup(23, GPIO.OUT, initial=GPIO.LOW) #replace 1 with pin number
+freq = 10
+deli = 1/(2*freq)
 
-###########
+try:
+    while True:
+        GPIO.output(23, GPIO.HIGH)
+        sleep(deli) #in seconds
+        GPIO.output(23, GPIO.LOW)
+        sleep(deli)
+        
+except KeyboardInterrupt:
+    GPIO.cleanup()
+
+
 import wiringpi
-#wiringpi.wiringPiSetup() #by physical board number
-#wiringpi.wiringPiSetupGpio() #by GPIO pin number
-wiringpi.softToneCreate(1)
-frequency = 1000
-wiringpi.softToneWrite(1, frequency)
+wiringpi.wiringPiSetup() #by physical board number
+wiringpi.wiringPiSetupGpio() #by GPIO pin number
+wiringpi.softToneCreate(23)
+frequency = 1
+wiringpi.softToneWrite(23, frequency)
 #while true....
 time.sleep(20) #keep program running so code stays running
-wiringpi.softToneWrite(1, 0) #shut off LED
-
+wiringpi.softToneWrite(23, 0) #shut off LED
+"""'''
 ###########
 import pigpio
 pi = pigpio.pi() #sleect local pi for control
-frequency = 1000
-pi.set_PWM_frequency(1, frequency)
-dutyCycleValue = 255 #0 is always off, 255 is always on
-pi.set_PWM_dutycycle(1, dutyCycleValue)
+#pi = pigpio.pi('soft', 8888)
+frequency = 1
+pi.set_PWM_frequency(23, frequency)
+dutyCycleValue = 127 #0 is always off, 255 is always on
+pi.set_PWM_dutycycle(23, dutyCycleValue)
 #while true....
 time.sleep(20) #keep program running so code stays running
-pi.set_PWM_dutycycle(1, 0)
+pi.set_PWM_dutycycle(23, 0)
