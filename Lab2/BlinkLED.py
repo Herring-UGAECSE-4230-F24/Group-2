752ed7f88ab6f5e967a0e7f8ef4e2e002ad281ce
Hannahmho <155788231+Hannahmho@users.noreply.github.com>
2024-09-05 17:27:09 -0400

    Update BlinkLED.py
    Added comments

--- Lab2/BlinkLED.py
+++ Lab2/BlinkLED.py
@@ -1,17 +1,19 @@
+
 import RPi.GPIO as GPIO
 import time
 from time import sleep
-
+#----------RPI.GPIO------------
+# Initialize GPIOS
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(24, GPIO.OUT, initial=GPIO.LOW) #replace 1 with pin number
-freq = 1
-deli = 1/(2*freq)
+freq = 1 # Measured frequency in Hz
+deli = 1/(2*freq) # Calculate delay from frequency value
 
 try:
     while True:
         GPIO.output(24, GPIO.HIGH)
-        sleep(deli) #in seconds
+        sleep(deli) # delay in seconds
         GPIO.output(24, GPIO.LOW)
         sleep(deli)
         
@@ -19,32 +21,31 @@ except KeyboardInterrupt:
     GPIO.cleanup()
 
 '''
-
+#----------WIRINGPI------------
 import wiringpi
-wiringpi.wiringPiSetup() #by physical board number
-wiringpi.wiringPiSetupGpio() #by GPIO pin number
+wiringpi.wiringPiSetup() #Initialize by physical board number
+wiringpi.wiringPiSetupGpio() #Initialize by by GPIO pin number
 wiringpi.softToneCreate(24)
-frequency = 10
+frequency = 10 # set frequency
 try:
     wiringpi.softToneWrite(24, frequency)
     while True:
-        time.sleep(100)
+        time.sleep(100) # How long program will run
 except KeyboardInterrupt:
-    wiringpi.softToneWrite(24, 0)
-    wiringpi.softToneStop(24)
+    wiringpi.softToneWrite(24, 0) # Stop program
+    wiringpi.softToneStop(24) # Clean up
 
-###########
-
+#----------PIGPIO------------
 import pigpio
 pi = pigpio.pi() #sleect local pi for control
 #pi = pigpio.pi('soft', 8888)
-frequency = 10
+frequency = 10 # Set frequency
 try:
-    pi.set_PWM_frequency(24, frequency)
+    pi.set_PWM_frequency(24, frequency) # Set frequency to pin
     dutyCycleValue = 127 #0 is always off, 255 is always on
-    pi.set_PWM_dutycycle(24, dutyCycleValue)
+    pi.set_PWM_dutycycle(24, dutyCycleValue) # Set duty cycle
     while True:
-        time.sleep(200) #keep program running so code stays running
+        time.sleep(200) # Keep program running
 except KeyboardInterrupt:
-    pi.set_PWM_dutycycle(24, 0)
-    '''
+    pi.set_PWM_dutycycle(24, 0) # Stop program
+'''
