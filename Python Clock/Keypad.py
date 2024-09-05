2174adeec4d8aad2dd15b0efd6477a370867855e
Hannahmho <155788231+Hannahmho@users.noreply.github.com>
2024-09-05 17:18:41 -0400

    Update Keypad.py
    Added comments

--- Python Clock/Keypad.py
+++ Python Clock/Keypad.py
@@ -14,6 +14,7 @@ Y2 = 13
 Y3 = 19
 Y4 = 26
 
+# Set up GPIOS
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(X1, GPIO.OUT)
@@ -29,7 +30,7 @@ GPIO.setup(Y4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 def readKeypad(rowNum, char):
     GPIO.output(rowNum, GPIO.HIGH)
     curVal = None
-    if GPIO.input(Y1)==0:
+    if GPIO.input(Y1)==0: # If button pressed, input is LOW
         curVal=char[0]
     if GPIO.input(Y2)==0:
         curVal=char[1]
@@ -42,13 +43,13 @@ def readKeypad(rowNum, char):
 
 try:
     while True:
-        key = None
+        key = None # Variable to store the key value to print
         key = readKeypad(X1, ["1","2","3","A"]) or key
         key = readKeypad(X2, ["4","5","6","B"]) or key
         key = readKeypad(X3, ["7","8","9","C"]) or key
         key = readKeypad(X4, ["*","0","#","D"]) or key
         if key:
-            print("Key pressed: " + key + "\n")
+            print("Key pressed: " + key + "\n") # Print the key value
         sleep(0.2)
 except KeyboardInterrupt:
     GPIO.cleanup()
