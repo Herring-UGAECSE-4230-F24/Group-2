56a451275d42427f61c90a14eea11a2858dc862f
Hannahmho <155788231+Hannahmho@users.noreply.github.com>
2024-09-10 23:37:38 -0400

    Update add.s
    changed adds for assmebly

--- arthimeticAssembly/add/add.s
+++ arthimeticAssembly/add/add.s
@@ -3,6 +3,6 @@
 _start:
         @This stores 20 in R1 then add 10 from that and stores the remaining amount in R0
         MOV R1, #0x14
-        ADD R0, R1, #-0x15
+        ADDS R0, R1, #-0x14
         MOV R7, #1
         SWI 0
