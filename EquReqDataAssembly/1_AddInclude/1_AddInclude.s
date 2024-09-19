f776fbaa8eabd08ab187212b5202114a74bd6f52
hannahmho <hmh78733@uga.edu>
2024-09-18 22:09:04 -0400

    Finished Assembly -All

--- EquReqDataAssembly/1_AddInclude/1_AddInclude.s
+++ EquReqDataAssembly/1_AddInclude/1_AddInclude.s
@@ -8,7 +8,7 @@
 .global _start
 
 _start:
-        @This stores 20 in R1 then subtracts 10 from that and stores the remaining amount in R0
+        @This stores 20 in R1 then adds 10 from that and stores the remaining amount in R0
         MOV R1, #0x14
         ADD R0, R1, #0xA
         MOV R7, #sys_exit
