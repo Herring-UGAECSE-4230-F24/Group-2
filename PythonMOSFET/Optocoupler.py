6e574a87452bab8af671d2a5e9645569f2e22b61
hannahmho <hmh78733@uga.edu>
2024-10-30 20:38:58 -0400

    Added comments

--- PythonMOSFET/Optocoupler.py
+++ PythonMOSFET/Optocoupler.py
@@ -2,23 +2,22 @@ import RPi.GPIO as GPIO
 import time
 from time import sleep
 
+# Initialize GPIO 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-#GPIO Number
-channel = 26
 
-#Changeable
-frequency = 5
+channel = 26 #GPIO Number
+frequency = .1 # Frequency sent to the motor that can be changed
 
+# Setup GPIO
 GPIO.setup(channel, GPIO.OUT)
 GPIO.output(channel, GPIO.HIGH)
-pwm=GPIO.PWM(channel, frequency)
-
+pwm=GPIO.PWM(channel, frequency) # Setup GPIO pin for PWM output
 
 try:
-    while True:
-        #Duty Cycle
-        pwm.start(50)
+    while True: # Loop to continuously run motor
+        pwm.start(50) # Start PWM output and set duty Cycle
+
 except KeyboardInterrupt:
-    GPIO.cleanup()
-    pwm.stop()
+    GPIO.cleanup() # Clean up GPIO
+    pwm.stop() # Stop PWM
